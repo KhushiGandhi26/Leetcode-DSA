7a146b07b3b13bb7f5b32614f8c5d809638125bd
Khushi Gandhi <91653619+KhushiGandhi26@users.noreply.github.com>
2023-06-19 01:00:59 +0530

    Time: 8 ms (62.09%), Space: 7.1 MB (72.71%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
-    bool safe(int row,int col,vector<string> &board,int n){
-        int r=row;
-        int c=col;
-        while(c>=0){
-            if(board[r][c]=='Q')return false;
-            c--;
-        }
-        c=col;
-        while(r>=0 && c>=0){
-            if(board[r][c]=='Q')return false;
-            r--;c--;
-        }
-        r=row;
-        c=col;
-        while(r<n && c>=0){
-            if(board[r][c]=='Q')return false;
-            r++;c--;
-        }
-        return true;
-    }
-    void solve(int col,vector<string> &board,vector<vector<string>> &ans,int n){
+    void solve(int col,vector<string> &board,vector<vector<string>> &ans,int n,vector<int> &ver,vector<int> &upDiag,vector<int> &downDiag){
         if(col==n){
             ans.push_back(board);
             return;
         }
         for(int row=0;row<n;row++){
-            if(safe(row,col,board,n)==true){
+            if(ver[row]==0 && upDiag[row+col]==0 && downDiag[n-1+col-row]==0){
                 board[row][col]='Q';
-                solve(col+1,board,ans,n);
+                ver[row]=1;
+                upDiag[row+col]=1;
+                downDiag[n-1+col-row]=1;
+                solve(col+1,board,ans,n,ver,upDiag,downDiag);
                 board[row][col]='.';
+                ver[row]=0;
+                upDiag[row+col]=0;
+                downDiag[n-1+col-row]=0;
             }
             
         }
@@ -41,7 +27,10 @@ public:
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        solve(0,board,ans,n);
+        vector<int> row(n,0);
+        vector<int> upDiag(2*n-1,0);
+        vector<int> downDiag(2*n-1,0);
+        solve(0,board,ans,n,row,upDiag,downDiag);
         return ans;
     }
 };
