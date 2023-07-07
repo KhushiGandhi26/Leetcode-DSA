a787a3db22971340d69da3a5ad7790bf85042de1
Khushi Gandhi <91653619+KhushiGandhi26@users.noreply.github.com>
2023-07-07 08:49:02 +0530

    Time: 12 ms (17.58%), Space: 8.9 MB (48.75%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<int> prev(n,0);
         for(int i=0;i<n;i++){
-            dp[n-1][i]=triangle[n-1][i];   //Base case of recursion
+            prev[i]=triangle[n-1][i];   //Base case of recursion
         }
         
         for(int i=n-2;i>=0;i--){
+            vector<int> curr(n,0);
             for(int j=i;j>=0;j--){
-                int down=triangle[i][j]+dp[i+1][j];
-                int diago=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,diago);
+                int down=triangle[i][j]+prev[j];
+                int diago=triangle[i][j]+prev[j+1];
+                curr[j]=min(down,diago);
             }
+            prev=curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
