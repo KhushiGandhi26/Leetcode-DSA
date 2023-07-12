f1a000c3fffb8b753cc2af4bd1979d3e58225472
Khushi Gandhi <91653619+KhushiGandhi26@users.noreply.github.com>
2023-07-12 12:08:28 +0530

    Time: 478 ms (27.88%), Space: 13 MB (73.12%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    bool func(int i, int k, vector<int> &arr,vector<vector<int>> &dp) {
-        if(k==0)return true;
-        if(i==0)return (k==arr[0]);
-        if(dp[i][k]!=-1)return dp[i][k];
-        int notTake=func(i-1,k,arr,dp);
-        int take=false;
-        if(k>=arr[i])take=func(i-1,k-arr[i],arr,dp);
-        return dp[i][k]=take||notTake;
+    bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<vector<bool>> dp(n,vector<bool>(k+1,0));
+    if(arr[0]<=k)dp[0][arr[0]]=true;
+    for(int i=0;i<n;i++)dp[i][0]=true;
+    for(int ind=1;ind<n;ind++){
+        for(int tar=1;tar<=k;tar++){
+            bool notTake=dp[ind-1][tar];
+            bool take=false;
+            if(arr[ind]<=tar)take=dp[ind-1][tar-arr[ind]];
+            dp[ind][tar]=take|notTake;
+        }
+    }
+    return dp[n-1][k];
 }
     bool canPartition(vector<int>& nums) {
         int total=0;
@@ -15,7 +20,6 @@ public:
             total+=nums[i];
         }
         if(total%2!=0)return false;
-        vector<vector<int>> dp(nums.size(),vector<int>((total/2)+1,-1));
-        return func(nums.size()-1,total/2,nums,dp);
+        return subsetSumToK(nums.size(),total/2,nums);
     }
 };
