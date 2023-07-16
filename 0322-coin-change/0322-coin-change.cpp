3d378132d36565d089f8747d4050712dbefbc0a8
Khushi Gandhi <91653619+KhushiGandhi26@users.noreply.github.com>
2023-07-16 15:08:33 +0530

    Time: 180 ms (18.90%), Space: 45.1 MB (37.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int func(int ind,int tar,vector<int> &coins,vector<vector<int>>&dp){
-        if(ind==0){
-            if(tar%coins[0]==0)return tar/coins[0];
-            return 1e9;
-        }
-        if(dp[ind][tar]!=-1)return dp[ind][tar];
-        int notTake=0+func(ind-1,tar,coins,dp);
-        int take=1e9;
-        if(coins[ind]<=tar)take=1+func(ind,tar-coins[ind],coins,dp);
-        return dp[ind][tar]=min(take,notTake);
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans=func(n-1,amount,coins,dp);
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0]==0)dp[0][t]=t/coins[0];
+            else dp[0][t]=1e9;
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=amount;t++){
+                int notTake=0+dp[i-1][t];
+                int take=1e9;
+                if(coins[i]<=t)take=1+dp[i][t-coins[i]];
+                dp[i][t]=min(take,notTake);
+            }
+        }
+        int ans=dp[n-1][amount];
         if(ans>=1e9)return -1;
         return ans;
     }
