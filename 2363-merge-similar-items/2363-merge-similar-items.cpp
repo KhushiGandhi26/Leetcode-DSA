da171fd163274de2505cfa041701f0d90fb0a360
Khushi Gandhi <91653619+KhushiGandhi26@users.noreply.github.com>
2023-07-19 00:47:10 +0530

    Time: 30 ms (84.63%), Space: 16.8 MB (74.15%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.cpp
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -5,10 +5,8 @@ public:
         for(int i=0;i<items1.size();i++){
             m.insert({items1[i][0],items1[i][1]});
         }
-        
         for(int i=0;i<items2.size();i++){
             if(m.find(items2[i][0])!=m.end()){
-                int a=m[items2[i][0]];
                 m[items2[i][0]]+=items2[i][1];
             }
             else{
@@ -17,10 +15,7 @@ public:
         }
         vector<vector<int>> ans;
         for(auto e:m){
-            vector<int> aa;
-            aa.push_back(e.first);
-            aa.push_back(e.second);
-            ans.push_back(aa);
+            ans.push_back({e.first,e.second});
         }
         return ans;
     }
