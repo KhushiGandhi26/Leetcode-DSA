7315a106878538236c1ac8243e10bc26b25155cd
Khushi Gandhi <91653619+KhushiGandhi26@users.noreply.github.com>
2023-07-30 18:07:11 +0530

    Time: 3 ms (45.54%), Space: 6.3 MB (80.32%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string ans;
-        int n=word1.length();
-        int m=word2.length();
-        for(int i=0;i<min(n,m);i++){
-            ans+=word1[i];
-            ans+=word2[i];
+        string ans="";
+        int i1=0,i2=0;
+        while(i1!=word1.length() && i2!=word2.length()){
+            ans+=word1[i1];
+            ans+=word2[i2];
+            i1++;
+            i2++;
         }
-        if(n>m){
-            for(int i=m;i<n;i++){
-                ans+=word1[i];
-            }
+        while(i1!=word1.length()){
+            ans+=word1[i1];
+            i1++;
         }
-        else if(m>n){
-            for(int i=n;i<m;i++){
-                ans+=word2[i];
-            }
+        while(i2!=word2.length()){
+            ans+=word2[i2];
+            i2++;
         }
         return ans;
     }
