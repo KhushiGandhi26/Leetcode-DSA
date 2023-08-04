27f82c786846dc491c56df9619a36753ad7f9958
Khushi Gandhi <91653619+KhushiGandhi26@users.noreply.github.com>
2023-08-04 12:07:26 +0530

    Time: 16 ms (66.11%), Space: 13.4 MB (18.40%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>> zeros;
         int n=matrix.size();
         int m=matrix[0].size();
+        vector<int> row(n,-1);
+        vector<int> col(m,-1);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    zeros.push_back({i,j});
+                    row[i]=0;
+                    col[j]=0;
                 }
             }
         }
-        for(auto e:zeros){
-            for(int i=0;i<n;i++){
-                matrix[i][e.second]=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]==0 || col[j]==0){
+                    matrix[i][j]=0;
+                }
             }
-            for(int i=0;i<m;i++){
-                matrix[e.first][i]=0;
-            }
-            // cout<<e.first<<" "<<e.second<<endl;
         }
-        
     }
 };
